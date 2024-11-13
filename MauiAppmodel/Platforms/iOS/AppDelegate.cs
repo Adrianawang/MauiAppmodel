1488a8826983f3341eb060c8c37a5a4be3d79b62
Adriana <carolinaadriana77@outlook.com>
2024-11-13 20:13:25 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppmodel/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppmodel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
